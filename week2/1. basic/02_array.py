000324384e0da9948a0c192ae7c2dca4feaa8af3
LeeKangHyun <a01057364452@gmail.com>
2026-08-28 22:57:24 +0900

    5번까지

--- week2/1. basic/02_array.py
+++ week2/1. basic/02_array.py
@@ -33,7 +33,7 @@
 
 def rotate_matrix_90(matrix):
     """
-    2차원 배열을 시계방향으로 90도 회전
+    2차원 배열을 시계방향으로 90도 회전 하는 함수
     
     Args:
         matrix: N x N 2차원 리스트
@@ -41,15 +41,25 @@ def rotate_matrix_90(matrix):
     Returns:
         회전된 2차원 리스트
     """
-    n = len(matrix)
+    n = len(matrix) #받는 배열의 개수? 구하는 거
+    
+
+    
     
     # TODO: n x n 크기의 새로운 배열을 생성하세요 (0으로 초기화)
-    pass
+    rotated = []
+    for i in range(n): #3번 
+        row = []
+        for j in range(n):
+            row.append(0)
+        rotated.append(row)
         
     # TODO: 원본 배열의 각 요소를 회전된 위치에 배치하세요
     # 힌트: (i, j) 위치의 요소는 회전 후 (j, n-1-i) 위치로 이동
-    pass
-    
+    for i in range(n):
+        for j in range(n):
+            rotated[j][n-1-i] = matrix[i][j]
+
     return rotated
 
 def print_matrix(matrix):
--- week2/1. basic/03_python_dict.py
+++ week2/1. basic/03_python_dict.py
@@ -42,14 +42,24 @@ def find_above_average_students(students):
         tuple: (평균 점수, 평균 이상 학생 이름 리스트)
     """
     # TODO: 모든 학생의 점수를 리스트로 추출하세요
-    pass
-    
+    b = []
+    for i in range(len(students)):
+        a = students[i]["score"]
+        b.append(a)
     # TODO: 평균 점수를 계산하세요
-    pass
-    
+    average = sum(b)
+    average /= len(students)
+    # for i in range(len(b)):
+    #     sum += b[i]
+
+    # average
+    above_average_students = []
     # TODO: 평균 이상인 학생들의 이름을 리스트로 추출하세요
-    pass
-    
+    for i in range(len(students)):
+        if students[i]["score"]>= average:
+            above_average_students.append(students[i]["name"])    
+
+
     return average, above_average_students
 
 # 테스트 케이스
